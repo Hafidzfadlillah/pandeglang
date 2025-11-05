12675109f8570a84ef23bec92772051e8e6ebe8c
HafidzFadlillah <mhafidzfdh@gmail.com>
2025-11-05 15:45:34 +0700

    benerin

--- vite.config.js
+++ vite.config.js
@@ -12,20 +12,3 @@ export default defineConfig({
     ],
 });
 
-@theme {
-  --font-sans: InterVariable, sans-serif;
-  --font-sans--font-feature-settings: 'cv02', 'cv03', 'cv04', 'cv11';
-}
-
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  // ...
-}
